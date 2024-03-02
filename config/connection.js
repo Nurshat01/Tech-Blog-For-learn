f01d9fe9b01a9c703354477f5489f78900a5c323
Adam <nuradam3033@gmail.com>
2024-03-02 14:34:27 -0600

    Pushing to Heroku updates

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,9 @@
-// Load environment variables from .env file
+
 require('dotenv').config();
 
-// Import Sequelize
+
 const Sequelize = require('sequelize');
 
-// Create a Sequelize instance based on environment variables
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
@@ -16,10 +15,9 @@ const sequelize = process.env.JAWSDB_URL
       },
     });
 
-// Export the Sequelize instance
+
 module.exports = sequelize;
 
-// Log database information from .env file (optional)
 console.log('---> Database Name from .env:', process.env.DB_NAME);
 console.log('---> Database User from .env:', process.env.DB_USER);
 console.log('---> Database Password from .env:', process.env.DB_PW);
